1c2897cd0368e9879692755b4beeb22c3597019f
TeacherTay <133699296+TeacherTay@users.noreply.github.com>
2025-06-24 20:09:54 -0300

    fdgdfgdf

---
+++ script.js
@@ -0,0 +1,25 @@
+function inserirNome(){
+   let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   elemento.textContent = nomeUsuario;
+}
+inserirNome();
+
+function mudarCor(){
+   const elemento=document.getElementById("nome-usuario");
+   elemento.style.color="red";
+}
+
+mudarCor();
+
+let linguagens = ["Javascript", "Python", "C"];
+//índice             0              1       2
+
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
+
+//mostrar no console no navegador
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[1];

d01629deeaeb403f79214de006fb0156208dc7d2
TeacherTay <133699296+TeacherTay@users.noreply.github.com>
2025-06-30 18:44:53 -0300

    DHGFHGFH

--- script.js
+++ script.js
@@ -3,23 +3,19 @@ function inserirNome(){
    let elemento = document.querySelector("#nome-usuario");
    elemento.textContent = nomeUsuario;
 }
+
 inserirNome();
+
+let linguagens = ["Javascript", "Python", "C"];
+console.log(linguagens[0]);
+console.log(linguagens[1]);
+console.log(linguagens[2]);
 
 function mudarCor(){
    const elemento=document.getElementById("nome-usuario");
    elemento.style.color="red";
 }
-
 mudarCor();
-
-let linguagens = ["Javascript", "Python", "C"];
-//índice             0              1       2
-
-console.log(linguagens[0]);
-console.log(linguagens[1]);
-console.log(linguagens[2]);
-
 //mostrar no console no navegador
-
 const item = document.querySelector("#lista");
 item.textContent = linguagens[1];
